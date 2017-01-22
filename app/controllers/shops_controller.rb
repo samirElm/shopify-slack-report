526af0b7fd90b22e5a2827692a3b4c75f9391b24
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2017-01-22 17:54:05 +0100

    Edit/Update for shop slack_webhook_url

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,10 +4,27 @@ class ShopsController < ShopifyApp::AuthenticatedController
   end
 
   def edit
-    @shop = Shop.current
+    load_shop
   end
 
   def update
-    @shop = Shop.current
+    load_shop
+    @shop.update(shop_params)
+
+    if @shop.save
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:slack_webhook_url)
+  end
+
+  def load_shop
+    @shop = Shop.find(params[:id])
   end
 end
