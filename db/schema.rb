676850ae4720db68d75f113aa8a42fa8cb215cfc
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2017-02-11 14:30:34 +0100

    Add provider and access_token to shops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122161553) do
+ActiveRecord::Schema.define(version: 20170211132951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170122161553) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slack_webhook_url", default: ""
+    t.string   "access_token",      default: ""
   end
 
   add_index "shops", ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true, using: :btree
