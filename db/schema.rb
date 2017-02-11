e10d31735fc0080057cd261db47b54c6694257d5
Samir El Maoujoudi <samir.elmaoujoudi@gmail.com>
2017-02-11 15:48:17 +0100

    Remove slack_webhook_url attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211134332) do
+ActiveRecord::Schema.define(version: 20170211144722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shops", force: :cascade do |t|
-    t.string   "shopify_domain",                 null: false
-    t.string   "shopify_token",                  null: false
+    t.string   "shopify_domain",              null: false
+    t.string   "shopify_token",               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slack_webhook_url", default: ""
-    t.string   "access_token",      default: ""
-    t.string   "provider",          default: ""
+    t.string   "access_token",   default: ""
+    t.string   "provider",       default: ""
   end
 
   add_index "shops", ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true, using: :btree
